962fe39cf666a5cc6d5db3d41d9ebd5bfce73ff2
fabio <hsu.fabio13@gmail.com>
2024-08-15 05:51:34 -0700

    Init commit

--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,23 @@
-export const pagesSounds: Array<Array<string>> = [
-  [
-    require("./assets/audio/19/Landing.mp3"),
-    require("./assets/audio/1/Slide 1.mp3"),
-  ],
-  [
-    require("./assets/audio/19/Landing.mp3"),
-    require("./assets/audio/1/Slide 1.mp3"),
-  ],
+import Logo from "./logo.svg";
+
+type PageAsset = {
+  imageSrc: string;
+  sounds: string[];
+};
+
+export const pagesAssets: PageAsset[] = [
+  {
+    imageSrc: Logo,
+    sounds: [
+      require("./assets/audio/19/Landing.mp3"),
+      require("./assets/audio/1/Slide 1.mp3"),
+    ],
+  },
+  {
+    imageSrc: Logo,
+    sounds: [
+      require("./assets/audio/19/Landing.mp3"),
+      require("./assets/audio/1/Slide 1.mp3"),
+    ],
+  },
 ];
